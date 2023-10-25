cf61ac1e4eb9fc4f069572cfd4c2f8e55f8ba240
gglamer <azamat201760@yandex.ru>
2023-10-25 20:50:56 +0500

    verify token fix

--- model/token.py
+++ model/token.py
@@ -1,10 +1,5 @@
-
-from datetime import datetime
-
-from jwt import decode
+from jwt import decode, ExpiredSignatureError
 from pydantic import BaseModel, field_validator
-
-from core.exceptions import UnAuthorized
 
 
 class JWToken(BaseModel):
@@ -17,12 +12,12 @@ class JWToken(BaseModel):
     @field_validator('token')
     @classmethod
     def validator_token(cls, token: str) -> str:
-        t = decode(token, algorithms=['HS256'], options = {
-            'verify_signature': False,
-        })
-        exp = t.get('exp')
-        if exp and exp < datetime.now().timestamp():
+        try:
+            decode(token, algorithms=['HS256'], options = {
+                'verify_signature': False,
+                'verify_exp': True
+            })
+        except ExpiredSignatureError:
             cls.active = False
-            raise UnAuthorized()
 
         return token
